22a038f2691b5e1ba382c66f38ff96dbb04717d2
Paulo Schneider <paulo.schneider@gmail.com>
2017-04-04 13:23:55 +0100

    Experimenting with data strucutre for calendar

--- fcal.go
+++ fcal.go
@@ -7,6 +7,12 @@ import (
 	"time"
 )
 
+type cal struct {
+	month string
+	year  int
+	weeks [][]int
+}
+
 func startOfMonth(t time.Time) time.Time {
 	return t.AddDate(0, 0, 1-t.Day())
 }
@@ -43,6 +49,28 @@ func breakIntoWeeks(dates []time.Time) [][]time.Time {
 	return weeks
 }
 
+func printCalendar(c cal) {
+	header := fmt.Sprintf("%s %d", c.month, c.year)
+	fmt.Printf("%*s\n", 10+len(header)/2, header)
+	fmt.Println("Su Mo Tu We Th Fr Sa")
+
+	for j, week := range c.weeks {
+		var buffer bytes.Buffer
+
+		if j == 0 {
+			for i := 0; i <= 6-len(week); i++ {
+				buffer.WriteString("   ")
+			}
+		}
+
+		for _, day := range week {
+			buffer.WriteString(fmt.Sprintf("%2d ", day))
+		}
+
+		fmt.Println(buffer.String())
+	}
+}
+
 func main() {
 	fmt.Println(calendar.Thing())
 	now := time.Now()
@@ -67,4 +95,17 @@ func main() {
 
 		fmt.Println(buffer.String())
 	}
+
+	c := cal{year: 2017,
+		month: "April",
+		weeks: [][]int{
+			[]int{1},
+			[]int{2, 3, 4, 5, 6, 7, 8},
+			[]int{9, 10, 11, 12, 13, 14, 15},
+			[]int{16, 17, 18, 19, 20, 21, 22},
+			[]int{23, 24, 25, 26, 27, 28, 29},
+			[]int{30},
+		},
+	}
+	printCalendar(c)
 }
