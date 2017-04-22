fd98fa5b52e33c6443527a36c7fcd0f623930c3d
Paulo Schneider <paulo.schneider@gmail.com>
2017-04-22 16:40:55 +0100

    Parse iso date

--- fcal.go
+++ fcal.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"flag"
 	"github.com/phss/fcal/calendar"
+	"log"
 	"time"
 )
 
 func main() {
-	c := calendar.CalendarMonthFrom(time.Now())
+	date := parseOptions()
+	c := calendar.CalendarMonthFrom(date)
 	calendar.PrintMonth(c)
+}
+
+func parseOptions() time.Time {
+	var dateStr string
+	flag.StringVar(&dateStr, "d", time.Now().Format("2006-01-02"), "ISO date of the day or month to be displayed")
+	flag.Parse()
+
+	date, err := time.Parse("2006-01-02", dateStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return date
 }
